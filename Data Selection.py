f473a75ea57f22a6daf70d561d58121769674eb1
pritikanaik1024 <121875645+pritikanaik1024@users.noreply.github.com>
2024-08-05 15:18:08 +0530

    Update Data Selection.py

--- Data Selection.py
+++ Data Selection.py
@@ -1,17 +1,13 @@
 import pandas as pd
-
 # Load the CSV file with tab delimiter and low_memory=False
 df = pd.read_csv('foodfacts.csv', delimiter='\t', on_bad_lines='skip', low_memory=False)
-
 # Print initial columns
 print("Initial columns:")
 print(df.columns)
-
 # List of columns to drop
 columns_to_drop = [
     ['creator', 'url'],
 ]
-
 # Drop columns if they exist in the DataFrame
 for cols in columns_to_drop:
     cols_to_drop = [col for col in cols if col in df.columns]
